b5116234994eaf7fdb26df3c3b45ab1c07287fa1
alfor2a <alexandre_fortier@hotmail.com>
2023-03-17 22:51:10 -0400

    mesure des 2 ai en simultané

--- Prise_mesure.py
+++ Prise_mesure.py
@@ -1,6 +1,7 @@
 import numpy as np
-import time 
-import nidaqmx
+import time ; from tkinter import *
+import nidaqmx ; from nidaqmx import stream_readers;
+from nidaqmx.stream_readers import AnalogSingleChannelReader, AnalogMultiChannelReader
 from nidaqmx.constants import (BridgeConfiguration, VoltageUnits, 
                                BridgeUnits, AcquisitionType)
 import matplotlib.pyplot as plt
@@ -21,70 +22,60 @@ Chercher de l'info pour de la lecture en simultané de plusieurs ports analogiqu
 
 ###############################################################################################################################################
 
-#Conditions d'échantillonnage
 
+#Conditions d'échantillonnage
 def analog_setup(freq = float, id_ai0 = str, id_ai1 = str, bool_ai0 = bool, bool_ai1 = bool, T_max = float):
 
     task = nidaqmx.Task()
-    
+    cas1 = None ; cas2 = None ; cas3 = None
     if (bool_ai0 == True) and (bool_ai1 == True):
         task.ai_channels.add_ai_voltage_chan(
             physical_channel=id_ai0, min_val=0.0,max_val=2.0,units=VoltageUnits.VOLTS) #init. AI0
         task.ai_channels.add_ai_voltage_chan(
             physical_channel=id_ai1, min_val=0.0,max_val=2.0,units=VoltageUnits.VOLTS) #init. AI1
+        cas1 = True
     elif (bool_ai0 == True) and (bool_ai1 == False):
         task.ai_channels.add_ai_voltage_chan(
             physical_channel=id_ai0, min_val=0.0,max_val=2.0,units=VoltageUnits.VOLTS) #init. AI0
+        cas2 = True
     elif (bool_ai0 == False) and (bool_ai1 == True):
         task.ai_channels.add_ai_voltage_chan(
             physical_channel=id_ai1, min_val=0.0,max_val=2.0,units=VoltageUnits.VOLTS) #init. AI1
+        cas3 = True
         
     task.timing.cfg_samp_clk_timing(freq, sample_mode = AcquisitionType.CONTINUOUS)    #Cette ligne doit être après l'init. des AI
-
+    
+    #reader = AnalogMultiChannelReader(task.in_stream) [read alternatif]
+    #data = np.ndarray([2,])  [read alternatif]
     task.start()
 
     T = 0
     while T < 1000:                                 # Modifier 1000 pour T_max
-        data = task.read(number_of_samples_per_channel = 1)     #Afficher data sur GUI
-        print(data, "V")
+        data = task.read(number_of_samples_per_channel = 1)
+
+        #a) reader.read_one_sample(data)  [read alternatif]
+        #b) Afficher data sur GUI
+
         #convertir les tensions list en float
-
+        if cas1 == True:
+            V0 = data[0][0]
+            V1 = data[1][0]
+            print(V0,"v   ",V1,"v")
+        elif cas2 == True:
+            V0 = data[0]
+            print(V0,"V")
+        elif cas3 == True:
+            V1 = data[0]
+            print(V1,"V")
         #Insérer équation de température en fonction de voltage provenant de l'étallonnage
-
         T = T + 1
 
     task.stop()
     task.close()
 
 
-
-analog_setup(40.0, "myDAQ1/ai0", "myDAQ1/ai1", True, False, 40.0)
-
+analog_setup(30.0, "myDAQ1/ai0", "myDAQ1/ai1", True, True, 40.0)
 
 
 
 
-
-
-
-
-"""task = nidaqmx.Task()
-
-    task.ai_channels.add_ai_voltage_chan(
-        physical_channel="myDAQ1/ai0", min_val=0.0,max_val=2.0,units=VoltageUnits.VOLTS) #init. AI0
-    task.ai_channels.add_ai_voltage_chan(
-        physical_channel="myDAQ1/ai1", min_val=0.0,max_val=2.0,units=VoltageUnits.VOLTS) #init. AI1
-
-    task.timing.cfg_samp_clk_timing(10, sample_mode = AcquisitionType.CONTINUOUS) #
-
-    task.start()
-
-    sample = 0
-    while sample <1000:
-        data = task.read(number_of_samples_per_channel = 1)
-        print(data, "V")
-        sample = sample + 1
-    #task.wait_until_done()
-
-    task.stop()
-    task.close()"""
