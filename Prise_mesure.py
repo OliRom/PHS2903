2fb832636179debd0af86a96f5807d6ef3d4ff0c
alfor2a <alexandre_fortier@hotmail.com>
2023-03-20 12:00:38 -0400

    Master

--- Prise_mesure.py
+++ Prise_mesure.py
@@ -30,20 +30,20 @@ def analog_setup(freq = float, id_ai0 = str, id_ai1 = str, bool_ai0 = bool, bool
     
     # Ouverture/fermeture des deux ports analogiques
 
-   # cas1 = None ; cas2 = None ; cas3 = None
-    #if (bool_ai0 == True) and (bool_ai1 == True):
-    task.ai_channels.add_ai_voltage_chan(
-        physical_channel=id_ai0, min_val=0.0,max_val=2.0,units=VoltageUnits.VOLTS) #init. AI0
-    task.ai_channels.add_ai_voltage_chan(
-        physical_channel=id_ai1, min_val=0.0,max_val=2.0,units=VoltageUnits.VOLTS) #init. AI1
-    cas1 = True
-    #elif (bool_ai0 == True) and (bool_ai1 == False):
-    #    task.ai_channels.add_ai_voltage_chan(
-      #      physical_channel=id_ai0, min_val=0.0,max_val=2.0,units=VoltageUnits.VOLTS) #init. AI0
-     #   cas2 = True
-   # elif (bool_ai0 == False) and (bool_ai1 == True):
-   #     task.ai_channels.add_ai_voltage_chan(
-       #     physical_channel=id_ai1, min_val=0.0,max_val=2.0,units=VoltageUnits.VOLTS) #init. AI1
+    cas1 = None ; cas2 = None ; cas3 = None
+    if (bool_ai0 == True) and (bool_ai1 == True):
+        task.ai_channels.add_ai_voltage_chan(
+            physical_channel=id_ai0, min_val=0.0,max_val=2.0,units=VoltageUnits.VOLTS) #init. AI0
+        task.ai_channels.add_ai_voltage_chan(
+            physical_channel=id_ai1, min_val=0.0,max_val=2.0,units=VoltageUnits.VOLTS) #init. AI1
+        cas1 = True
+    elif (bool_ai0 == True) and (bool_ai1 == False):
+        task.ai_channels.add_ai_voltage_chan(
+            physical_channel=id_ai0, min_val=0.0,max_val=2.0,units=VoltageUnits.VOLTS) #init. AI0
+        cas2 = True
+    elif (bool_ai0 == False) and (bool_ai1 == True):
+        task.ai_channels.add_ai_voltage_chan(
+            physical_channel=id_ai1, min_val=0.0,max_val=2.0,units=VoltageUnits.VOLTS) #init. AI1
      #   cas3 = True
         
     task.timing.cfg_samp_clk_timing(freq, sample_mode = AcquisitionType.CONTINUOUS)    #Cette ligne doit être après l'init. des AI
@@ -63,7 +63,7 @@ def analog_setup(freq = float, id_ai0 = str, id_ai1 = str, bool_ai0 = bool, bool
        # if cas1 == True:
         V0 = data[0][0]
         V1 = data[1][0]
-            #print(V0,"v   ",V1,"v")
+        print(V0,"v   ",V1,"v")
         return V0, V1
            
         #elif cas2 == True:
@@ -80,7 +80,7 @@ def analog_setup(freq = float, id_ai0 = str, id_ai1 = str, bool_ai0 = bool, bool
     task.stop()
     task.close()
     
-def voltage_divider(R1 = float, VS = float):
+"""def voltage_divider(R1 = float, VS = float):
     V0, V1 =  analog_setup(10.0, "myDAQ1/ai0", "myDAQ1/ai1", True, True, 40.0)
     RT0 = R1*(1/((VS/V0)-1.0))
     RT1 = R1*(1/((VS/V1)-1.0))
@@ -92,7 +92,7 @@ def temperature(a,b,c):
     T1 = 1/(a+(b*mt.log(RT1))+(c*(mt.log(voltage_divider(115000.0,15.0)))**3))
     print(T0, T1)
 
-temperature(*para.coef_init_guess[:3])
+temperature(*para.coef_init_guess[:3])"""
 
 
 
