154acb2c948e6682d7fbfff7f7b4feb0546e8d7e
Germain Desloges <59802152+Beanazril@users.noreply.github.com>
2023-03-27 15:59:27 +0000

    ajouté foolsafe sur procedure etalonnage

--- Procedure_etalonnage.py
+++ Procedure_etalonnage.py
@@ -5,10 +5,17 @@ import Parameters as param
 
 i= int(input('Numero de la thermistance (1 ou 2): '))
 courbe = []
-print(courbe)
+chiffre=[f'{j}' for j in range(10)]
 
 while True:
-    T= float(input('Temperature affichage RTD: '))
+    while True:
+        T=input('Temperature affichage RTD: ')
+        if T=='':
+            print('Mauvaise valeur, recommencer')
+        elif T[0] not in chiffre or T[-1] not in chiffre:
+            print('Mauvaise valeur, recommencer')
+        else:
+            break
     V=ut.mesure_v(para.daq_ports[f"thermi_{i}"])
     courbe.append((V,T))
     if str(input('Q pour quitter, enter pour continuer: '))=='Q':
