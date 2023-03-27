5ce95c2d3e5393eaad410bc06d6488e096f3522a
Germain Desloges <59802152+Beanazril@users.noreply.github.com>
2023-03-27 15:36:31 +0000

    Code d'étalonnage

--- Procedure_etalonnage.py
+++ Procedure_etalonnage.py
@@ -3,24 +3,18 @@ import pandas as pd
 import numpy as np
 import Parameters as param
 
-therm=1
+i= int(input('Numero de la thermistance (1 ou 2): '))
+courbe = []
+print(courbe)
 
-def V(x): #tension de thermistance sur myDAQ
-    return x
+while True:
+    T= float(input('Temperature affichage RTD: '))
+    V=ut.mesure_v(para.daq_ports[f"thermi_{i}"])
+    courbe.append((V,T))
+    if str(input('Q pour quitter, enter pour continuer: '))=='Q':
+        break
 
-def T(x): #temperature selon RTD
-    return x
-
-
-list = []
-print(list)
-
-tuple=(1,2)
-
-for i in range(10):
-    list.append((V(i),T(i)))
-
-df = pd.DataFrame(list, columns=["V","T"])
+df = pd.DataFrame(courbe, columns=["V","T"])
 print(df)
 
-df.to_csv(param.etal_data_file_paths[therm])
+df.to_csv(param.etal_data_file_paths[f'thermi_{i}'])
