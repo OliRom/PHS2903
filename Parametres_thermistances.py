c5dc9d023006616f404dc480b0101b18a29d6aa6
Olivier Romain <olivierromain140@gmail.com>
2023-04-05 11:09:56 -0400

    Détermination des parmaètres des thermistances.

--- Parametres_thermistances.py
+++ Parametres_thermistances.py
@@ -12,16 +12,19 @@ def find_coef(therm, plot_regression=False):
 
     data = pd.read_csv(data_path)
     
-    coef, cov = optimize.curve_fit(ut.v_to_temp, data["V"], data["T"], param.coef_init_guess)
+    coef, cov = optimize.curve_fit(ut.v_to_temp, data["V"], data["T"], p0=param.coef_init_guess, maxfev=1000000)
+
+    print(coef)
+    print(param.coef_init_guess)
     
     np.savez(save_path, coef=coef, cov=cov)
 
     if plot_regression:
-        x = np.arange(1, 11)
-        plt.plot(x, ut.v_to_temp(x, *coef), label="Régression")
-        plt.scatter(data["V"], data["T"], label="Données")
+        x = np.linspace(0.5, 1.5, 1000)
+        plt.plot(x, ut.v_to_temp(x, *coef), label="Régression", color="blue")
+        plt.scatter(data["V"], data["T"], label="Données", color="red", s=2)
         plt.legend()
         plt.show()
 
 if __name__ == "__main__":
-    find_coef("thermi_1", True)
+    find_coef("thermi_ext", True)
